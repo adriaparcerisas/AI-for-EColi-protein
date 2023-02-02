f95c0c7b9d737794c108885aba6a793c92386d8c
Adrià Parcerisas <adria.parcerisas.albes@gmail.com>
2023-02-02 18:39:48 +0100

    Update Using_AI_to_detect_ECOLI_sites.py

--- Using_AI_to_detect_ECOLI_sites.py
+++ Using_AI_to_detect_ECOLI_sites.py
@@ -17,15 +17,9 @@ import altair as alt
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import favicon
 import math
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
-html_code = """
-<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
-"""
-
-st.markdown(html_code, unsafe_allow_html=True)
 
 
 
@@ -93,36 +87,23 @@ st.markdown("""
 1. Title: Protein Localization Sites
 
 
-2. Creator and Maintainer:
-	     Kenta Nakai
-             Institue of Molecular and Cellular Biology
-	     Osaka, University
-	     1-3 Yamada-oka, Suita 565 Japan
-	     nakai@imcb.osaka-u.ac.jp
-             http://www.imcb.osaka-u.ac.jp/nakai/psort.html
-   Donor: Paul Horton (paulh@cs.berkeley.edu)
-   Date:  September, 1996
-   See also: yeast database
+2. Creator and Maintainer: Kenta Nakai, Institue of Molecular and Cellular Biology, Osaka, University, nakai@imcb.osaka-u.ac.jp http://www.imcb.osaka-u.ac.jp/nakai/psort.html
+Donor: Paul Horton (paulh@cs.berkeley.edu)
+Date:  September, 1996
+See also: yeast database
 
 
-3. The references below describe a predecessor to this dataset and its 
-development. They also give results (not cross-validated) for classification 
-by a rule-based expert system with that version of the dataset.
+3. The references below describe a predecessor to this dataset and its development. They also give results (not cross-validated) for classification by a rule-based expert system with that version of the dataset.
 
-Reference: "Expert Sytem for Predicting Protein Localization Sites in 
-           Gram-Negative Bacteria", Kenta Nakai & Minoru Kanehisa,  
-           PROTEINS: Structure, Function, and Genetics 11:95-110, 1991.
+Reference: "Expert Sytem for Predicting Protein Localization Sites in Gram-Negative Bacteria", Kenta Nakai & Minoru Kanehisa, PROTEINS: Structure, Function, and Genetics 11:95-110, 1991.
 
-Reference: "A Knowledge Base for Predicting Protein Localization Sites in
-	   Eukaryotic Cells", Kenta Nakai & Minoru Kanehisa, 
-	   Genomics 14:897-911, 1992.
+Reference: "A Knowledge Base for Predicting Protein Localization Sites in Eukaryotic Cells", Kenta Nakai & Minoru Kanehisa, Genomics 14:897-911, 1992.
 
 
-5. Number of Instances:  336 for the E.coli dataset and 
+5. Number of Instances:  336 for the E.coli dataset 
 
 
-6. Number of Attributes.
-         for E.coli dataset:  8 ( 7 predictive, 1 name )
+6. Number of Attributes. for E.coli dataset:  8 ( 7 predictive, 1 name )
 
 	     
 7. Attribute Information.
@@ -131,31 +112,14 @@ Reference: "A Knowledge Base for Predicting Protein Localization Sites in
   2.  mcg: McGeoch's method for signal sequence recognition.
   3.  gvh: von Heijne's method for signal sequence recognition.
   4.  lip: von Heijne's Signal Peptidase II consensus sequence score.
-           Binary attribute.
   5.  chg: Presence of charge on N-terminus of predicted lipoproteins.
-	   Binary attribute.
-  6.  aac: score of discriminant analysis of the amino acid content of
-	   outer membrane and periplasmic proteins.
+  6.  aac: score of discriminant analysis of the amino acid content of outer membrane and periplasmic proteins.
   7. alm1: score of the ALOM membrane spanning region prediction program.
-  8. alm2: score of ALOM program after excluding putative cleavable signal
-	   regions from the sequence.
+  8. alm2: score of ALOM program after excluding putative cleavable signal regions from the sequence.
 
 
 
 8. Missing Attribute Values: None.
-
-
-9. Class Distribution. The class is the localization site. Please see Nakai &
-		       Kanehisa referenced above for more details.
-
-  cp  (cytoplasm)                                    143
-  im  (inner membrane without signal sequence)        77               
-  pp  (perisplasm)                                    52
-  imU (inner membrane, uncleavable signal sequence)   35
-  om  (outer membrane)                                20
-  omL (outer membrane lipoprotein)                     5
-  imL (inner membrane lipoprotein)                     2
-  imS (inner membrane, cleavable signal sequence)      2
   """
   )
 
